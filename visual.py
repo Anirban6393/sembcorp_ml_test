0f8d7815061c140547a0a36921fc31a92b11078f
Anirban Kar Chaudhuri <44681864+Anirban6393@users.noreply.github.com>
2021-10-04 08:03:07 +0800

    Add files via upload

---
+++ visual.py
@@ -0,0 +1,38 @@
+import dash
+from dash import html
+from dash import dcc
+from dash.dependencies import Input, Output
+import plotly.express as px
+from sqlalchemy import *
+import numpy as np
+import pandas as pd
+
+engine = create_engine('sqlite:///pyakc.db', echo=False)
+table_name='sensor'
+df=pd.read_sql_query("SELECT * FROM {}".format(table_name), con=engine)
+
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Dropdown(
+        id="ticker",
+        options=[{"label": x, "value": x} 
+                 for x in df.columns[2:]],
+        value=df.columns[1],
+        clearable=False,
+    ),
+    dcc.Graph(id="time-series-chart"),
+])
+
+
+@app.callback(
+    Output("time-series-chart", "figure"), 
+    [Input("ticker", "value")])
+def display_time_series(ticker):
+    fig = px.line(df, x='created_timestamp', y=ticker)
+    return fig
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True, port=1000)
